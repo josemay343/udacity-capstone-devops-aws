a24d1455276e77d45fbad1328a70610a6b1e8081
Jose <jose.may343@hotmail.com>
2021-08-18 20:12:07 -0400

    add new job

---
+++ upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=josemay343/capstone-app
+
+# Step 2:  
+# Authenticate & tag
+docker login --username josemay343 && docker image tag capstone-app $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
